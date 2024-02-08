729933181d840255495102491ad831e9239aaff9
KoalaOnDrugs <david12788@gmail.com>
2024-02-08 17:24:27 +0200

    fixed for phone and add kennslider

--- components/links/Links.jsx
+++ components/links/Links.jsx
@@ -45,7 +45,7 @@ export default function Links({ fadeIn, timeout }) {
           }}
         >
         
-          <Box
+          {/* <Box
             sx={{
               border: "solid 0px #d12e27",
               transition: "border-width 0.3s linear",
@@ -63,7 +63,7 @@ export default function Links({ fadeIn, timeout }) {
                 className={styles.instagram_icon}
               />
             </Link>
-          </Box>
+          </Box> */}
 
           <Box
             sx={{
